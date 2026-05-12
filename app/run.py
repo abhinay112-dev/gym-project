3bc97cdc92dc59e61ce526fe6a48649e4c42a045
abhinay112-dev <abhinaysai71@gmail.com>
2026-05-12 12:06:29 +0530

    deployment

--- app/run.py
+++ app/run.py
@@ -1,6 +1,10 @@
-from app import create_app
+from app import create_app, db
+from flask_migrate import upgrade
 
-flask_app =create_app()
+flask_app = create_app()
 
-if __name__== '__main__':
-    flask_app.run(host='0.0.0.0',debug=True)
+with flask_app.app_context():
+    upgrade()
+
+if __name__ == '__main__':
+    flask_app.run(host='0.0.0.0', debug=True)
